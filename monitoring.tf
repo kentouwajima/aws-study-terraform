573ba004b66d243fc7bfd3d4f217cc7f87d3f4a2
kentouwajima <u.kento6366@gmail.com>
2026-01-20 15:05:31 +0900

    Terraformのフォーマット修正

--- monitoring.tf
+++ monitoring.tf
@@ -19,11 +19,11 @@ resource "aws_sns_topic_subscription" "cpu_alarm_email" {
 # CloudWatch Alarm (EC2 CPU)
 # ----------------------------
 resource "aws_cloudwatch_metric_alarm" "ec2_cpu_high" {
-  alarm_name          = "aws-study-ec2-cpu-high"
-  alarm_description   = "Alarm when CPU exceeds 10%"
-  namespace           = "AWS/EC2"
-  metric_name         = "CPUUtilization"
-  
+  alarm_name        = "aws-study-ec2-cpu-high"
+  alarm_description = "Alarm when CPU exceeds 10%"
+  namespace         = "AWS/EC2"
+  metric_name       = "CPUUtilization"
+
   # 監視対象の特定（compute.tfで作ったEC2のIDを参照）
   dimensions = {
     InstanceId = aws_instance.study_ec2.id
@@ -36,18 +36,18 @@ resource "aws_cloudwatch_metric_alarm" "ec2_cpu_high" {
   threshold           = 10
 
   # アラーム状態になった時のアクション（SNSへの通知）
-  alarm_actions       = [aws_sns_topic.cpu_alarm_topic.arn]
+  alarm_actions = [aws_sns_topic.cpu_alarm_topic.arn]
 }
 
 # ----------------------------
 # CloudWatch Alarm (WAF Blocked)
 # ----------------------------
 resource "aws_cloudwatch_metric_alarm" "waf_blocked_alarm" {
-  alarm_name          = "aws-study-waf-blocked"
-  alarm_description   = "Alarm when WAF blocks any request"
-  namespace           = "AWS/WAFV2"
-  metric_name         = "BlockedRequests"
-  
+  alarm_name        = "aws-study-waf-blocked"
+  alarm_description = "Alarm when WAF blocks any request"
+  namespace         = "AWS/WAFV2"
+  metric_name       = "BlockedRequests"
+
   dimensions = {
     WebACL = aws_wafv2_web_acl.study_web_acl.name
     Region = "ap-northeast-1"
@@ -61,5 +61,5 @@ resource "aws_cloudwatch_metric_alarm" "waf_blocked_alarm" {
   threshold           = 0
   treat_missing_data  = "notBreaching"
 
-  alarm_actions       = [aws_sns_topic.cpu_alarm_topic.arn]
+  alarm_actions = [aws_sns_topic.cpu_alarm_topic.arn]
 }

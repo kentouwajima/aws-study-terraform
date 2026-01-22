d197a7bb4e509c0d12595fdcbf9de9df478cd47b
kentouwajima <u.kento6366@gmail.com>
2026-01-22 14:55:56 +0900

    Ansible実行時に動的にSGを開放する設定を追加

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,8 @@ output "ec2_public_ip" {
   description = "Public IP of the EC2 instance"
   value       = aws_instance.study_ec2.public_ip
 }
+
+output "security_group_id" {
+  description = "ID of the Security Group"
+  value       = aws_security_group.ec2_sg.id
+}
